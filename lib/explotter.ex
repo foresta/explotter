07ab0029c19f76026610378b864c9deca758f271
kazuki morita <kz.morit@gmail.com>
2017-11-29 13:45:58 +0900

    initial commit

---
+++ lib/explotter.ex
@@ -0,0 +1,18 @@
+defmodule Explotter do
+  @moduledoc """
+  Documentation for Explotter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Explotter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
